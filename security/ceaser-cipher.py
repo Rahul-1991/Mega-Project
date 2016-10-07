f901106ca2bf5326d645ba2483b35e8da4d20e37
Rahul-1991 <rahul.srivastava@students.iiit.ac.in>
2016-10-07 21:57:59 +0530

    completed ceaser-cipher for capital letters

---
+++ security/ceaser-cipher.py
@@ -0,0 +1,27 @@
+class CeaserCipher(object):
+
+    def get_key_from_user(self):
+        while True:
+            key = raw_input('Enter key for ceaser cipher : ')
+            if key.isdigit():
+                break
+        return int(key)
+
+    def get_string_from_user(self):
+        input_string = raw_input('Enter string to encrypt or decrypt : ')
+        return input_string
+
+    def perform_cipher(self):
+        key = self.get_key_from_user()
+        input_string = self.get_string_from_user()
+        decrypted_string = ''
+        for i in input_string:
+            if ord(i) + key > ord('Z'):
+                decrypted_string += chr(key + ord(i) + ord('A') - ord('Z') - 1)
+            else:
+                decrypted_string += chr(ord(i) + key)
+
+        print 'Input String : %s' %input_string
+        print 'decrypted String : %s' %decrypted_string
+
+cipher = CeaserCipher().perform_cipher()
